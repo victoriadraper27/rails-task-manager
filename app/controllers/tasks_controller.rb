f270489d2afb9aa28f4a0ac7ed6f4baa80dcad4d
Victoria Draper <victoria.draper27@gmail.com>
2020-11-17 15:42:15 +0900

    Finished Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,6 +35,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
