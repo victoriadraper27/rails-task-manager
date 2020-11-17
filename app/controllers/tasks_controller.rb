408a7588c395bd5843551f527d91381de7ef7093
Victoria Draper <victoria.draper27@gmail.com>
2020-11-17 12:50:49 +0900

    Added Task Router

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+before_action :find_task, except:[:index, :new, :create]
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
